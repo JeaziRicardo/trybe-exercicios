2d28afd8310e53f9489ca6c9f36f000b237b295a
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-03 20:03:34 -0300

    resolução de alguns exercícios do dia

---
+++ fundamentos/04-intro-javascript-lógica-programacao/02-array-e-loop-for/scripts.js
@@ -0,0 +1,48 @@
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (let index = 0; index < groceryList.length; index += 1) {
+//   console.log(groceryList[index]);
+// }
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+// for (let namel of names) {
+//   console.log(namel);
+// }
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+// for (let number of numbers) {
+//   console.log(number);
+// }
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+// for (let soma of numbers) {
+//   soma += 1;
+//   console.log(soma);
+// }
+// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+// let soma = 0;
+// let médiaAritmética = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index]; 
+//   médiaAritmética = soma / numbers.length;
+// }
+// console.log("Média Aritmética = " + médiaAritmética);
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+// let soma = 0;
+// let médiaAritmética = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index]; 
+//   médiaAritmética = soma / numbers.length;
+// } 
+// if (médiaAritmética <= 20) {
+//   console.log("valor menor ou igual a 20");
+// } else {
+//   console.log("valor maior que 20");
+// }
+// Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+let maior = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > maior) {
+// } NÃOTERMINEI
