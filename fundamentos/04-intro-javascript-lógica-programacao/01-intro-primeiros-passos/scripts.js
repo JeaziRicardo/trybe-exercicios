34a4fbfdb47a8a75a83510236b8d499e2c635636
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-03 11:27:40 -0300

    exercícios do dia

--- fundamentos/04-intro-javascript-lógica-programacao/01-intro-primeiros-passos/scripts.js
+++ fundamentos/04-intro-javascript-lógica-programacao/01-intro-primeiros-passos/scripts.js
@@ -1,6 +1,110 @@
-const base = 5;
-let height = 8;
-const area = base * height;
-const perimeter = (base**2) + (height**2)
+const a = 45;
+const b = 45;
+const c = 91;
 
-console.log(perimeter);
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+
+// if (a > b) {
+//   console.log("'a' é o maior número");
+// } else {
+//   console.log("'b' é o maior número");
+// }
+
+// if (a > b && a > c) {
+//   console.log('O maior número é: ' + a + ' (a)');
+// } else if (b > a && b > c) {
+//   console.log('O maior número é: ' + b + ' (b)');
+// } else {
+//   console.log('O maior número é: ' + c + ' (c)');
+// };
+
+// if (a > 0) {
+//   console.log("positivo");
+// } else if (a < 0) {
+//   console.log("negativo");
+// }  else {
+//   console.log("zero");
+// }
+
+// let angulos = a + b + c;
+// let todosOsAngulos = a > 0 && b > 0 && c > 0;
+// if (todosOsAngulos){
+//   if (angulos === 180){
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   };
+// } else {
+//   console.log("erro")
+// }
+  
+// let pieci = "queen";
+// if (pieci == "king") {
+//   console.log("one square in any direction");
+// } else if (pieci == "queen") {
+//   console.log("all square in any direction");
+// } else if (pieci == "knight") {
+//   console.log("in an L shape");
+// } else if (pieci == "bishop") {
+//   console.log("diagonally");
+// } else if (pieci == "rook") {
+//   console.log("horizontally or vertically");
+// } else if (pieci == "pawns") {
+//   console.log("vertically forward one square, with the option to move two squares if they have not yet moved.");
+// } else {
+//   console.log("erro");
+// }
+
+// const nota = 65;
+
+// if (nota < 0 || nota > 100) {
+//   console.log("Erro, nota incorreta!");
+// } else if (nota >= 90) {
+//   console.log("A");
+// } else if (nota >= 80) {
+//   console.log("B");
+// } else if (nota >= 70) {
+//   console.log("C");
+// } else if (nota >= 60) {
+//   console.log("D");
+// } else if (nota >= 50) {
+//   console.log("E");
+// } else {
+//   console.log("F");
+// }
+
+// if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+// if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+// const valorCusto = 10;
+// const valorVenda = 30;
+// if (valorCusto >= 0 && valorVenda >=0) {
+//   const valorCustoTotal = valorCusto * 1.2;
+//   const lucroTotal = (valorVenda - valorCustoTotal) * 1000
+//   console.log(lucroTotal)
+// } else {
+//   console.log("valor invalido")
+// }
+
+// const salárioBruto = 1500.00;
+// const inss = 0.00;
+// if (salárioBruto <= 1556.94) {
+//   inss = inss + 0.08;
+// }
+// NÃO TERMINEI
+
+
+
