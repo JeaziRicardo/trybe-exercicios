0e9509ec9e3b4448d8e4505cdfb9271fafc24b50
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-04 18:59:39 -0300

    exercicio 04

--- fundamentos/04-intro-javascript-lógica-programacao/03-log-de-program-e-algoritmos/scripts.js
+++ fundamentos/04-intro-javascript-lógica-programacao/03-log-de-program-e-algoritmos/scripts.js
@@ -13,20 +13,33 @@
 // }
 
 // Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor.
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let biggestWord = array[0];
-let lettestword = array[0]
-for (let index = 1; index < array.length; index += 1) {
-  if (array[index].length > biggestWord.length){
-    biggestWord = array[index];
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let biggestWord = array[0];
+// let lettestword = array[0]
+// for (let index = 1; index < array.length; index += 1) {
+//   if (array[index].length > biggestWord.length){
+//     biggestWord = array[index];
+//   }
+// }
+// for (let index = 1; index < array.length; index += 1) {
+//   if (array[index].length < lettestword.length){
+//     lettestword = array[index];
+//   }
+// }
+// console.log(biggestWord);
+// console.log(lettestword);
+
+// escreva um algoritmo que retorne o maior número primo entre 0 e 50.
+let biggestPrimeNumber = 0;
+for (let currentNumber = 0; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
   }
 }
-for (let index = 1; index < array.length; index += 1) {
-  if (array[index].length < lettestword.length){
-    lettestword = array[index];
-  }
-}
-console.log(biggestWord);
-console.log(lettestword);
-
-
+console.log(biggestPrimeNumber);
