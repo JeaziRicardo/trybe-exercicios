0e0f566c0a93efb39f3c4efa60886cb1261875af
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-04 18:29:37 -0300

    exercicio 03

--- fundamentos/04-intro-javascript-lógica-programacao/03-log-de-program-e-algoritmos/scripts.js
+++ fundamentos/04-intro-javascript-lógica-programacao/03-log-de-program-e-algoritmos/scripts.js
@@ -6,8 +6,27 @@
 // console.log(fatorial);
 
 // desenvolva um algoritmo que é capaz de inverter uma palavra
-let word = 'trybe';
-let reverseWord = "";
-for (let index = word.length - 1; index >= 0; index -= 1) {
-  console.log(word[index]);
+// let word = 'trybe';
+// let reverseWord = "";
+// for (let index = word.length - 1; index >= 0; index -= 1) {
+//   console.log(word[index]);
+// }
+
+// Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor.
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let biggestWord = array[0];
+let lettestword = array[0]
+for (let index = 1; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length){
+    biggestWord = array[index];
+  }
 }
+for (let index = 1; index < array.length; index += 1) {
+  if (array[index].length < lettestword.length){
+    lettestword = array[index];
+  }
+}
+console.log(biggestWord);
+console.log(lettestword);
+
+
