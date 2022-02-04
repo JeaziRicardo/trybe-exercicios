8befa92649ac6992c2c3e18218512a0d3bd80926
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-04 16:04:38 -0300

    exercicio 02

--- fundamentos/04-intro-javascript-lógica-programacao/03-log-de-program-e-algoritmos/scripts.js
+++ fundamentos/04-intro-javascript-lógica-programacao/03-log-de-program-e-algoritmos/scripts.js
@@ -1,6 +1,13 @@
 // crie um algoritmo que retorne o fatorial de 10.
-let fatorial = 1;
-for (let index = 10; index > 0; index -= 1) {
-  fatorial *= index
+// let fatorial = 1;
+// for (let index = 10; index > 0; index -= 1) {
+//   fatorial *= index
+// }
+// console.log(fatorial);
+
+// desenvolva um algoritmo que é capaz de inverter uma palavra
+let word = 'trybe';
+let reverseWord = "";
+for (let index = word.length - 1; index >= 0; index -= 1) {
+  console.log(word[index]);
 }
-console.log(fatorial);
