49b7b47f03cb77c9c4a7f293671d5b29c1a9cab0
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-08 09:44:26 -0300

    exercicio 03

--- fundamentos/04-intro-javascript-lógica-programacao/04-objetos-e-funções/scripts.js
+++ fundamentos/04-intro-javascript-lógica-programacao/04-objetos-e-funções/scripts.js
@@ -7,6 +7,10 @@ let info = {
 console.log("Bem-vindo(a)", info.personagem);
 
 // 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. 
-info.recorrente = 'Sim',
+info.recorrente = 'Sim';
 console.log(info);
 
+// 3 - Faça um for/in que mostre todas as chaves do objeto.
+for (let key in info) {
+  console.log(key);
+}
