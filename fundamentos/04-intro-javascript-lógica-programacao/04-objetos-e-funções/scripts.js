284864f53dcc1924f6a91ca7bb0e83bdd347e7eb
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-08 09:37:16 -0300

    exercicio 02

--- fundamentos/04-intro-javascript-lógica-programacao/04-objetos-e-funções/scripts.js
+++ fundamentos/04-intro-javascript-lógica-programacao/04-objetos-e-funções/scripts.js
@@ -5,3 +5,8 @@ let info = {
 };
 // 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 console.log("Bem-vindo(a)", info.personagem);
+
+// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. 
+info.recorrente = 'Sim',
+console.log(info);
+
