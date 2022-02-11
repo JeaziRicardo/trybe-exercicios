62c65bb74f7b609199a02e83b1acf0f547d5f9ae
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-11 13:22:02 -0300

    adciona outro exercicio de fixaçao

--- fundamentos/05-DOM-eventos-e-web-storage/02-trabalhando-com-elementos/script.js
+++ fundamentos/05-DOM-eventos-e-web-storage/02-trabalhando-com-elementos/script.js
@@ -15,3 +15,23 @@ console.log(ondeEstou.nextSibling);
 console.log(ondeEstou.nextElementSibling);
 
 console.log(document.querySelector('#pai').lastElementChild.previousElementSibling); 
+
+// 1. Crie um irmão para `elementoOndeVoceEsta`.
+let pai = document.getElementById('pai');
+let irmaoElementoOndeEsta = document.createElement('section');
+irmaoElementoOndeEsta.id = 'irmaoElementoOndeEsta';
+pai.appendChild(irmaoElementoOndeEsta);
+
+// 2. Crie um filho para elementoOndeVoceEsta .
+let filhoElementoOndeEsta = document.createElement('section');
+filhoElementoOndeEsta.id = 'filhoElementoOndeEsta';
+ondeEstou.appendChild(filhoElementoOndeEsta);
+
+// 3. Crie um filho para primeiroFilhoDoFilho .
+let primeiroFilhoDoFilho = documento.getElementById('primeiroFilhoDoFilho');
+let filhoDoPrimeiroFilhoDoFilho = document.createElement('section');
+filhoDoPrimeiroFilhoDoFilho.id = 'filhoDoPrimeiroFilhoDoFilho';
+primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilho);
+
+// 4. A partir desse filho criado, acesse terceiroFilho .
+document.getElementById('filhoDoPrimeiroFilhoDoFilho').parentElement.parentElement.nextElementSibling;
