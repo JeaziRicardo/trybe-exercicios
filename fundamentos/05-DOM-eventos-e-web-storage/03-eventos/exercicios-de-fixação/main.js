a7a4f16adfb6a9ae2770504b163b72fda70a2309
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-13 22:07:55 -0300

    respondedo exercicio de fixaçao 04

--- fundamentos/05-DOM-eventos-e-web-storage/03-eventos/exercicios-de-fixação/main.js
+++ fundamentos/05-DOM-eventos-e-web-storage/03-eventos/exercicios-de-fixação/main.js
@@ -33,9 +33,13 @@ input.addEventListener('input', function(event) {
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+myWebpage.addEventListener('dblclick', function() {
+  window.open('https://jeaziricardo.github.io');
+});
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
 
 // Segue abaixo um exemplo do uso de event.target:
 
