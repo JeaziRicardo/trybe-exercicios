096908ba649a5a860fe22f65054f812ea9f52f08
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-05 12:29:38 -0300

    (exercicio 02) - cria uma lista de tarefas e exibe no navegador

--- front_end/10-intro-react/01-hello-word-no-react/fist-react/src/App.js
+++ front_end/10-intro-react/01-hello-word-no-react/fist-react/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Tasks = ['fazer o almoço', 'lavar os pratos', 'estudar'];
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   Tasks.map((item) => Task(item))
   );
 }
 
