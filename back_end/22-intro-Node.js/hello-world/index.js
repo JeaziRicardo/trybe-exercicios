0c7af4469e3215b851e56253cf18c3e577c622c8
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-07-27 14:24:41 -0300

    feat: cria um script para exibir o nome e a idade da pessoa

--- back_end/22-intro-Node.js/hello-world/index.js
+++ back_end/22-intro-Node.js/hello-world/index.js
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+const readline = require('readline-sync');
+
+const name = readline.question('What is your name? ');
+const age = readline.questionInt('how old are you? ');
+
+console.log(`Hello, ${name}! You are ${age} years old!`);
