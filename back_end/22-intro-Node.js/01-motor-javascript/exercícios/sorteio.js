b3f4ed9bf374cd290667f4819283410fc845ca0c
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-07-29 09:54:16 -0300

    feat: pergunta se quer jogar de novo e reinicia o jogo

--- back_end/22-intro-Node.js/01-motor-javascript/exercícios/sorteio.js
+++ back_end/22-intro-Node.js/01-motor-javascript/exercícios/sorteio.js
@@ -6,12 +6,22 @@ function numAleatorio() {
   return aleatorio;
 }
 
-function main() {
+function questao() {
   const numero = readline.questionInt('Digite um número inteiro entre 0 e 10 ');
   const aleatorio = numAleatorio();
 
   return numero === aleatorio
     ? console.log('Parabéns, número correto!') : console.log(`Opa, não foi dessa vez. O número era ${aleatorio}`);
+}
+
+function main() {
+  questao();
+
+  const resetar = readline.question('Digite "s" para jogar novamente ou qualquer outra tecla para sair ');
+
+  if (resetar !== 's') return console.log('Ok, até a próxima!');
+
+  main();
 }
 
 main();
