0f3d5913e322c5d67d61d880d0a08476a4c23b96
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-07-31 17:55:17 -0300

    feat: substitui o 'Nelson' pela 'Maggie'

--- back_end/22-intro-Node.js/02-fluxo-assincrono/Exercicio04/simpsons.js
+++ back_end/22-intro-Node.js/02-fluxo-assincrono/Exercicio04/simpsons.js
@@ -4,8 +4,9 @@ async function familySimpsons() {
   const read = await fs.readFile('./simpsonFamily.json', 'utf8');
 
   const simpsons = JSON.parse(read);
-  simpsons.push({ "id": "5", "name": "Nelson Muntz" });
-  const json = JSON.stringify(simpsons);
+  const up = simpsons.filter(({ name }) => name !== 'Nelson Muntz');
+  up.push({ "id": "5", "name": "Maggie Simpson" })
+  const json = JSON.stringify(up);
 
   await fs.writeFile('./simpsonFamily.json', json);
 }
