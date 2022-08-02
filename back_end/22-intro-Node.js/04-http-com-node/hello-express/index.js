bc1d59deaba76213f35d36b1f6ee1d6c18afb8b3
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-08-01 23:23:18 -0300

    feat: faz a requisição com filtros de nome e preço da comida

--- back_end/22-intro-Node.js/04-http-com-node/hello-express/index.js
+++ back_end/22-intro-Node.js/04-http-com-node/hello-express/index.js
@@ -32,6 +32,18 @@ app.get('/recipes-order', function (req, res) {
   res.json(sortRecipes);
 });
 
+app.get('/recipes/search', function (req, res) {
+  const { name } = req.query;
+  const filteredRecipes = recipes.filter((r) => r.name.includes(name));
+  res.status(200).json(filteredRecipes);
+});
+
+app.get('/recipes/search', function (req, res) {
+	const { name, maxPrice } = req.query;
+	const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price < Number(maxPrice));
+	res.status(200).json(filteredRecipes);
+});
+
 app.get('/recipes/:id', function (req, res) {
   const { id } = req.params;
   const recipe = recipes.find((r) => r.id === Number(id));
