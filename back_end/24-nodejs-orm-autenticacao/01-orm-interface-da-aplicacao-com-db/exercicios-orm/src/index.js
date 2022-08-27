e63001c819db868b7eee5bc1df9dcde4c3ddc9df
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-08-27 18:36:50 -0300

    fatal: exercício com erro 'require(..) is not a function'

--- back_end/24-nodejs-orm-autenticacao/01-orm-interface-da-aplicacao-com-db/exercicios-orm/src/index.js
+++ back_end/24-nodejs-orm-autenticacao/01-orm-interface-da-aplicacao-com-db/exercicios-orm/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const app = express();
+const BooksController = require('../controller/book.controller');
 require('dotenv').config();
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get('/books', BooksController.getAll);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
